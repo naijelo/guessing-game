dca30d9985a6e8c71e58c933429604022175f7c2
naijel <naijel.a@gmail.com>
2019-10-10 22:55:53 +0300

    Add: implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.right = max;
+        this.left = min;
     }
 
     guess() {
-
+//округлить
+        this.mid = Math.ceil((this.right + this.left) / 2);
     }
 
     lower() {
-
+// ответ БОЛЬШЕ нужного значения, сдвигаем границу влево
+        this.right = this.mid;
     }
 
     greater() {
-
+// ответ МЕНЬШЕ нужного значения, сдвигаем границу вправо
+        this.left = this.mid;
     }
 }
 
